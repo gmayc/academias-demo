0288ade0f4a3e2588cc8acf4fe71dc75e0bacdb3
Guillermo Maycotte <gmayc@MTYTM11786TT2.dir.svc.accenture.com>
2019-05-20 16:14:14 -0500

    simple router styled and working

--- src/components/SimpleNavBar.js
+++ src/components/SimpleNavBar.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../styles/navBar.css'
 
 const SimpleNavBar = () => {
   return (
       <div className="nav-bar">
-        <Link className="link" to="/">home</Link>
-        <Link className="link" to="/text">text</Link>
-        <Link className="link" to="/form">form</Link>
-        <Link className="link" to="/user">user</Link>
+        <div className="link-container">
+          <NavLink exact to="/" className="link"  activeClassName="selected">home</NavLink>
+        </div>
+        <div className="link-container">
+          <NavLink to="/text" className="link" activeClassName="selected">text</NavLink>
+        </div>
+        <div className="link-container">
+          <NavLink to="/form" className="link" activeClassName="selected">form</NavLink>
+        </div>
+        <div className="link-container" id="user-link">
+          <NavLink to="/user" className="link" activeClassName="selected">user</NavLink>
+        </div>
       </div>
   )
 }
