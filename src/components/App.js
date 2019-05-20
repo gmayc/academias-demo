f1a4a1a02f3ce8d0f0f5feb210060550af7cc395
Guillermo Maycotte <gmayc@MTYTM11786TT2.dir.svc.accenture.com>
2019-05-20 13:28:59 -0500

    added a simple nav component using css

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SimpleNavBar from './SimpleNavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleNavBar></SimpleNavBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SimpleNavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/navBar.css'
+
+const SimpleNavBar = () => {
+  return (
+    <div className="nav-bar">
+      <a>hi</a>
+      <a>hi</a>
+      <a>hi</a>
+    </div>
+  )
+}
+
+export default SimpleNavBar
