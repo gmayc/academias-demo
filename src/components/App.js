58a4a9fa945de99eec0c394168f14217e69fa851
Guillermo Maycotte <gmayc@MTYTM11786TT2.dir.svc.accenture.com>
2019-05-20 13:19:45 -0500

    NavBar with material ui

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Learn React
+      </header>
+      <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Typography from '@material-ui/core/Typography'
+import Toolbar from '@material-ui/core/Toolbar';
+
+const NavBar = () => {
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>
+
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default NavBar
